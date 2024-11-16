a377f0b03d541274fcb0b69232547e83a7d2e1cd
kevin artola <artolak6@gmail.com>
2024-11-15 23:15:39 -0300

    probando

--- libs/rute.php
+++ libs/rute.php
@@ -5,5 +5,5 @@ require_once 'app/controllers/taskapicontroller.php';
 $router = new Router();
                 // endpoint verbo   controller         metodo
 $router->addRoute('tareas', 'GET', 'taskapicontroller', 'getAll');
-
+//Estoy probando los diferentes comandos
 $router->route($_GET['resource'], $_SERVER  ['REQUEST_METHOD']);
